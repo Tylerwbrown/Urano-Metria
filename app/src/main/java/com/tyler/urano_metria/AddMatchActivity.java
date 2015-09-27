66f2a797b26c2d8a767b22d61ac9205d594165b3
Tylerwbrown <tyler@sliceofnet.com>
2015-09-26 23:46:37 -0400

    First steps towards API for matches.

---
+++ app/src/main/java/com/tyler/urano_metria/AddMatchActivity.java
@@ -0,0 +1,13 @@
+package com.tyler.urano_metria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddMatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_match);
+    }
+}
---
+++ app/src/main/java/com/tyler/urano_metria/Interactors/createMatch.java
@@ -0,0 +1,10 @@
+package com.tyler.urano_metria.Interactors;
+
+/**
+ * Created by Tyler on 9/26/2015.
+ */
+public class createMatch {
+    public createMatch() {
+
+    }
+}
