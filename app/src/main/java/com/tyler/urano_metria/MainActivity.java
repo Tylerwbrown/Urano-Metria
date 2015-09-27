20c378d5b4bd7e202fc24345c510a8a3da8ccf4c
Calvin Do <blazezekrom@gmail.com>
2015-09-26 23:47:13 -0400

    Login Activity Additions
    Created new class for logging in.

--- app/src/main/java/com/tyler/urano_metria/MainActivity.java
+++ app/src/main/java/com/tyler/urano_metria/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
